da72ff0160c41ea22b428e2cd28c628635c00a9c
Takaranoao <takaranoao@gmail.com>
2024-04-06 17:45:24 +0800

    :art: 修改按键绑定逻辑

--- common/src/main/java/com/takaranoao/donothitme/DNHKeyManager.java
+++ common/src/main/java/com/takaranoao/donothitme/DNHKeyManager.java
@@ -2,37 +2,38 @@ package com.takaranoao.donothitme;
 
 import com.takaranoao.donothitme.data.DNHKeyType;
 import net.minecraft.client.KeyMapping;
+import org.apache.commons.compress.utils.Lists;
 import org.lwjgl.glfw.GLFW;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class DNHKeyManager {
     private final DoNotHitMe mod;
-    private final KeyMapping swingMainKey;
-    private final KeyMapping swingOffKey;
-    private final KeyMapping enableKey;
+    private final List<DNHKey> keys = Lists.newArrayList();
 
     DNHKeyManager(DoNotHitMe mod) {
         this.mod = mod;
-        this.swingMainKey = new KeyMapping("key.donothitme.swing_main_hand", GLFW.GLFW_KEY_UNKNOWN, "key.category.donothitme");
-        this.swingOffKey = new KeyMapping("key.donothitme.swing_off_hand", GLFW.GLFW_KEY_UNKNOWN, "key.category.donothitme");
-        this.enableKey = new KeyMapping("key.donothitme.enable", GLFW.GLFW_KEY_UNKNOWN, "key.category.donothitme");
+        register("swing_main_hand", GLFW.GLFW_KEY_UNKNOWN, DNHKeyType.SWING_MAIN_HAND);
+        register("swing_off_hand", GLFW.GLFW_KEY_UNKNOWN, DNHKeyType.SWING_OFF_HAND);
+        register("enable", GLFW.GLFW_KEY_UNKNOWN, DNHKeyType.ENABLE);
+    }
+
+    public void register(String name, int defaultKey, DNHKeyType keyType) {
+        keys.add(new DNHKey(new KeyMapping("key.donothitme." + name, defaultKey, "key.category.donothitme"), keyType));
     }
 
     public List<KeyMapping> getKeyBindings() {
-        return Arrays.asList(swingMainKey, swingOffKey, enableKey);
+        return keys.stream().map(DNHKey::mapping).toList();
     }
 
     void onTick() {
-        if (swingMainKey.consumeClick()) {
-            mod.handleKeyPress(DNHKeyType.SWING_MAIN_HAND);
-        }
-        if (swingOffKey.consumeClick()) {
-            mod.handleKeyPress(DNHKeyType.SWING_OFF_HAND);
-        }
-        if (enableKey.consumeClick()) {
-            mod.handleKeyPress(DNHKeyType.ENABLE);
-        }
+        keys.forEach(key -> {
+            while (key.mapping.consumeClick()) {
+                mod.handleKeyPress(key.keyType);
+            }
+        });
+    }
+
+    record DNHKey(KeyMapping mapping, DNHKeyType keyType) {
     }
 }
